4719b78142afc3730b3497cbd20a0dbc2d386064
studentota2lvl <studentota2lvl@gmail.com>
2018-05-29 14:52:37 +0400

    homeWork2_2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+firstInputNumber: string;
+secondInputNumber: string;
+firstNumber: number;
+secondNumber: number;
+result: number;
+
+onCalculateButtonClick () {
+  this.typeReduction();
+  this.result = (this.firstNumber) + (this.secondNumber);
+  this.inputEraser();
+}
+
+typeReduction() {
+  this.firstNumber = Number(this.firstInputNumber);
+  this.secondNumber = Number(this.secondInputNumber);
+}
+
+inputEraser() {
+  this.firstInputNumber = '';
+  this.secondInputNumber = '';
+}
 }
